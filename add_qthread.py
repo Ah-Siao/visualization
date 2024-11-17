bef06cc6805e037935a9737248bd794e6ec8d9c8
Ah-Siao <128007349+Ah-Siao@users.noreply.github.com>
2024-11-17 10:52:07 +0800

    thread_progressbar

--- add_qthread.py
+++ add_qthread.py
@@ -1,112 +1,95 @@
-import sys
-import time  # Simulating heavy computations
-from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QProgressBar
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QProgressBar, QPushButton, QStatusBar, QLabel, QWidget, QHBoxLayout
 from PyQt5.QtCore import QThread, pyqtSignal
-from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtk import vtkMolecule, vtkActor,vtkRenderer,vtkMoleculeMapper,vtkPeriodicTable
+import time
+import sys
 
-class MoleculeWorker(QThread):
-    progress_updated = pyqtSignal(int)
-    molecule_ready = pyqtSignal(vtkMolecule)
 
-    def __init__(self, molecule_data):
-        super().__init__()
-        self.molecule_data = molecule_data
-
+class ThreadTask(QThread):
+    # Signal to update the progress bar
+    qthread_signal = pyqtSignal(int)
     def run(self):
-        molecule = vtkMolecule()
-        table = vtkPeriodicTable()
+        """The run method is executed in the new thread."""
+        max_value = 100
+        for i in range(max_value):
+            time.sleep(0.01)  # Simulate a time-consuming task
+            self.qthread_signal.emit(i + 1)
 
-        # Simulate processing large molecules
-        for i, atom in enumerate(self.molecule_data['atoms']):
-            time.sleep(0.1)  # Simulate delay
-            molecule.AppendAtom(table.GetAtomicNumber(atom[0]), *atom[1:])
-            self.progress_updated.emit(int((i + 1) / len(self.molecule_data['atoms']) * 100))
-
-        # Add bonds (optional for larger molecules, can also be part of atom processing)
-        for bond in self.molecule_data['bonds']:
-            molecule.AppendBond(bond[0], bond[1], bond[2])
-
-        self.molecule_ready.emit(molecule)
+class ThreadTask2(QThread):
+    # Signal for string
+    qthread_signal=pyqtSignal(str)
+    def run(self):
+        self.qthread_signal.emit('Start Searching...')
+        # put function here:
+        time.sleep(1)
+        self.qthread_signal.emit('Finish')
 
 
-class MoleculeWidget(QWidget):
+class MainWindowController(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Interactive Molecule Viewer with Progress")
-        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("TEST")
+        self.setGeometry(100, 100, 1200, 600)
+        # Create progress bar at statusbar region
+        self.progresscombo=QWidget()
+        self.progresslayout=QHBoxLayout()
 
-        layout = QVBoxLayout(self)
+        self.progressbar = QProgressBar(self)
+        self.progressbar.setMaximum(100)
+        #self.progressbar.setFixedWidth(150)
+        self.progressbar.hide()
+        self.progresslabel=QLabel()
+        self.progressbar.setFixedWidth(150)
+        self.progresslabel.hide()
+        self.progresslayout.addWidget(self.progresslabel,1)
+        self.progresslayout.addWidget(self.progressbar,1)
+        self.progresscombo.setLayout(self.progresslayout)
+        self.progresscombo.setFixedWidth(300)
+        # Create statusbar
+        statusbar=QStatusBar(self)
+        statusbar.addWidget(self.progresscombo,1)
+        self.setStatusBar(statusbar)
 
-        # Add label
-        layout.addWidget(QLabel("Rendering large molecules, please wait...", self))
+        # Create push button
+        self.pushbutton = QPushButton("Start Progress", self)
+        self.pushbutton.setGeometry(100, 150, 200, 50)
+        self.pushbutton.clicked.connect(self.button_click)
+        self.stop_progress()
 
-        # Progress bar
-        self.progress_bar = QProgressBar(self)
-        layout.addWidget(self.progress_bar)
+    def button_click(self):
+        self.progressbar.show()
+        # Create thread instance
+        #self.qthread=ThreadTask()
+        self.qthread = ThreadTask2()
+        # Connect signal from thread to the slot in main thread
+        # self.qthread.qthread_signal.connect(self.progress_changed)
+        self.qthread.qthread_signal.connect(self.progress_undetermine)
+        # Start the thread
+        self.qthread.start()
 
-        # VTK widget
-        self.vtk_widget = QVTKRenderWindowInteractor(self)
-        layout.addWidget(self.vtk_widget)
-
-        # VTK setup
-        self.renderer = vtkRenderer()
-        self.vtk_widget.GetRenderWindow().AddRenderer(self.renderer)
-
-        self.interactor = self.vtk_widget.GetRenderWindow().GetInteractor()
-
-        # Simulate large molecule rendering
-        self.init_large_molecule()
-
-    def init_large_molecule(self):
-        """Simulate initializing a large molecule."""
-        # Example molecule data (use a larger dataset for real cases)
-        molecule_data = {
-            "atoms": [("C", 0.0, 0.0, 0.0), ("C", 1.5, 0.0, 0.0), ("H", -1.0, 0.0, 0.0), ("H", 2.5, 0.0, 0.0)],
-            "bonds": [(0, 1, 1), (0, 2, 1), (1, 3, 1)]
-        }
-
-        self.worker = MoleculeWorker(molecule_data)
-        self.worker.progress_updated.connect(self.progress_bar.setValue)
-        self.worker.molecule_ready.connect(self.display_molecule)
-
-        self.worker.start()
-
-    def display_molecule(self, molecule):
-        """Render the molecule when it's ready."""
-        # Reset progress bar
-        self.progress_bar.setValue(0)
-
-        colors = vtkNamedColors()
-
-        # Create a molecule mapper and actor
-        mapper = vtkMoleculeMapper()
-        mapper.SetInputData(molecule)
-
-        actor = vtkActor()
-        actor.SetMapper(mapper)
-        actor.GetProperty().SetColor(colors.GetColor3d("SkyBlue"))
-
-        # Add actor to the renderer
-        self.renderer.AddActor(actor)
-        self.renderer.SetBackground(colors.GetColor3d("White"))
-
-        # Start VTK rendering
-        self.renderer.ResetCamera()
-        self.vtk_widget.GetRenderWindow().Render()
-        self.interactor.Initialize()
-
-
-def main():
-    app = QApplication(sys.argv)
-    widget = MoleculeWidget()
-    widget.show()
-    sys.exit(app.exec_())
+    def progress_changed(self, value):
+        # Update progress bar
+        self.progressbar.setValue(value)
+    
+    def stop_progress(self):
+        self.progressbar.setRange(0,100)
+        self.progressbar.setValue(100)
+    
+    def progress_undetermine(self,value):
+        self.progressbar.setRange(0,0)
+        self.progresslabel.show()
+        self.progresslabel.setText(value)
+        
+        if value =='Finish':
+            self.stop_progress()
+            self.progresslabel.setText(value)
 
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    widget = MainWindowController()
+    widget.show()
+    sys.exit(app.exec_())
